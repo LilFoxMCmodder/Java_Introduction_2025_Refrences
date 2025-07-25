87d30824fed89772e559a3784255c0f93d64069d
LilFoxMCmodder <fbi0101fbi@gmail.com>
2025-07-24 18:52:01 -0700

    update

--- src/lilfoxMCmodder/Main.java
+++ src/lilfoxMCmodder/Main.java
@@ -1,6 +1,14 @@
 package lilfoxMCmodder;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Minecraft!"); }
+
+
+
+
+
+
 }
